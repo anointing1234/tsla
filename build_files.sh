1c4c119783e3feea48e10e578ac5b01814ac1839
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-03-26 20:14:04 +0100

    commited

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 pip install -r requirements.txt
+
+# Apply database migrations
+python3.12 manage.py migrate
+
+# Collect static files
+mkdir -p staticfiles
 python3.12 manage.py collectstatic --noinput
