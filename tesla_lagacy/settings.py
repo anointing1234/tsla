363156f4df8609535cc9d290496bbaae2adebbc6
Agmetrix <124325944+anointing1234@users.noreply.github.com>
2026-01-18 19:46:27 +0100

    Update settings.py

--- tesla_lagacy/settings.py
+++ tesla_lagacy/settings.py
@@ -183,7 +183,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # else:
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ MEDIA_ROOT = '/data/media' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
